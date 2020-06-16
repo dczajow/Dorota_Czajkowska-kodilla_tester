c5fc677ec9531828dd5d97f339ebe43fad8983d8
dczajow <dorota.szubiczuk@gmail.com>
2020-06-16 16:24:26 +0200

    Notebook_03

--- src/main/java/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Dorota");
-        System.out.println(39);
-        System.out.println('C');
-    }
-}
