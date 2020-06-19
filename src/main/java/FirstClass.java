eb8100879e9c302e7d8ebf343c232d85e2b05769
dczajow <dorota.szubiczuk@gmail.com>
2020-06-19 15:26:41 +0200

    2.3. Ćwiczenie: instrukcje warunkowe

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,17 +1,23 @@
 public class FirstClass {
     public static void main(String[] args) {
 
-        Notebook notebook = new Notebook("600g", 1000);
+        Notebook notebook = new Notebook("600g", 900, 2018);
         System.out.println(notebook.weight + " " + notebook.price);
         notebook.checkPrice();
+        notebook.checkWeight();
+        notebook.yearVsPrice();
 
-        Notebook heavyNotebook = new Notebook("2000g", 1500);
+        Notebook heavyNotebook = new Notebook("2000g", 1500, 2020);
         System.out.println(heavyNotebook.weight + " " + heavyNotebook.price);
         heavyNotebook.checkPrice();
+        heavyNotebook.checkWeight();
+        heavyNotebook.yearVsPrice();
 
-        Notebook oldNotebook = new Notebook("1600g", 500);
+        Notebook oldNotebook = new Notebook("1600g", 500, 2019);
         System.out.println(oldNotebook.weight + " " + oldNotebook.price);
         oldNotebook.checkPrice();
+        oldNotebook.checkWeight();
+        oldNotebook.yearVsPrice();
 
     }
 }
--- src/main/java/Notebook.java
+++ src/main/java/Notebook.java
@@ -1,17 +1,47 @@
+import java.sql.SQLOutput;
+
 public class Notebook {
     String weight;
     int price;
+    int year;
 
-    public Notebook(String weight, int price) {
+    public Notebook(String weight, int price, int year) {
         this.weight = weight;
         this.price = price;
+        this.year = year;
     }
 
     public void checkPrice() {
-        if (this.price < 900) {
-            System.out.println("This notebook is cheap.");
-        } else {
+        if (this.price < 600) {
+            System.out.println("This notebook is very cheap.");
+        } else if(this.price > 600 && this.price < 1000) {
+            System.out.println("The price is good.");
+        }
+        else {
             System.out.println("This notebook is quite expensive.");
+        }
+    }
+
+    public void checkWeight() {
+        if (this.weight == "600g") {
+            System.out.println("This notebook is very light.");
+        } else if(this.weight == "2000g") {
+            System.out.println("This notebook is damn heavy.");
+        }
+        else {
+            System.out.println("This notebook is rather light.");
+        }
+    }
+
+    public void yearVsPrice() {
+        if(this.year <2020 && this.price < 1000) {
+            System.out.println("It has been released a while ago but it is afordable");
+        } else if(this.year <2020 && this.price > 1000) {
+            System.out.println("It has been released a while ago, yet it is quite expensive");
+        } else if(this.price < 1000){
+            System.out.println("It is a latest release. Great price");
+        } else {
+            System.out.println("It is a latest release. Quite expensive though.");
         }
     }
 }
