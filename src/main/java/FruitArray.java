bd7b4f795e4ae67f25762e47681edaa9fdf1536b
dczajow <dorota.szubiczuk@gmail.com>
2020-06-24 11:25:30 +0200

    2.4 Grades (Array)

---
+++ src/main/java/FruitArray.java
@@ -0,0 +1,9 @@
+public class FruitArray {
+    public static void main(String[] args) {
+        String [] fruits = {"apple", "pear", "banana", "orange"};
+
+        for(String nowaZmienna: fruits) {
+            System.out.println(nowaZmienna);
+        }
+    }
+}
