86e9a0201e0e378629b6a03a73cb6b7cf2b508b6
dczajow <dorota.szubiczuk@gmail.com>
2020-06-25 12:45:04 +0200

    2.5.2 Pętle (Random numbers min i max)

--- src/main/java/RandomNumbers.java
+++ src/main/java/RandomNumbers.java
@@ -6,16 +6,21 @@ aż ich suma przekroczy 5000. Napisz dwie metody zwracające największą i najm
 import java.util.Random;
 
 public class RandomNumbers {
-   public static void main(String[] args) {
-       Random generator = new Random();
-       int suma = 0;
-       do {
-           if(suma<5001) {
-               int wylosowanaLiczba = generator.nextInt(31);
-               suma += wylosowanaLiczba;
-           }
-           System.out.println(suma);
-       }
-       while (suma < 5001);
+    public static void main(String[] args) {
+        Random generator = new Random();
+        int suma = 0;
+        int min = 30;
+        int max = 0;
+        while (suma < 5001) {
+            int wylosowanaLiczba = generator.nextInt(31);
+            if (wylosowanaLiczba > max) {
+                max = wylosowanaLiczba; }
+            if (wylosowanaLiczba < min) {
+                min = wylosowanaLiczba; }
+            suma += wylosowanaLiczba;
+        }
+        System.out.println("Najniższa wylosowana liczba to: " +min);
+        System.out.println("Najwyższa wylosowana liczba to: " +max);
+        System.out.println("Suma; " +suma);
     }
 }
