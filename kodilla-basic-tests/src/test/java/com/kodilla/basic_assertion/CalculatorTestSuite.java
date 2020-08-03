19f9a670736e18cdfa96fd04ea324d4b6367a697
dczajow <dorota.szubiczuk@gmail.com>
2020-08-03 11:36:39 +0200

    JUnit

---
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -0,0 +1,33 @@
+package com.kodilla.basic_assertion;
+
+import org.junit.jupiter.api.Test;
+
+import static com.kodilla.basic_assertion.ResultChecker.assertEquals;
+
+public class CalculatorTestSuite {
+    @Test
+    public void testSum() {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int sumResult = calculator.sum(a, b);
+        assertEquals(13, sumResult);
+    }
+
+    @Test
+    public void testSub() {
+        Calculator calculator2 = new Calculator();
+        int a = 5;
+        int b = 2;
+        int subResult = calculator2.subtract(a, b);
+        assertEquals(3, subResult);
+    }
+
+    @Test
+    public void testSquared() {
+        Calculator calculator3 = new Calculator();
+        int a = 5;
+        int squaredResult = calculator3.squared(a);
+        assertEquals(15, squaredResult);
+    }
+}
