6af2e0145ba8332156a6722e5ba8820b44c6f5aa
dczajow <dorota.szubiczuk@gmail.com>
2020-08-03 09:10:25 +0200

    refaktoryzacja kodu

---
+++ kodilla-intro/src/main/java/Book.java
@@ -0,0 +1,24 @@
+public class Book {
+
+    private String author;
+    private String title;
+
+    private Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    public String toString() {
+        return this.author + " - " + this.title;
+    }
+
+    public static void main(String[] args) {
+        Book book = Book.of("Agnieszka Stein", "Dziecko z bliska");
+        System.out.println(book);
+    }
+
+    private static Book of(String author, String title) {
+        return new Book(author, title);
+    }
+
+}
