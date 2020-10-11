63edee3669a997b139efe7ca5317686d8ecb2d5b
Dorota Czajkowska-Szubiczuk <dorota.szubiczuk@gmail.com>
2020-10-11 15:39:43 +0200

    11.10

--- kodilla_stream/src/main/java/com/kodilla/stream/exceptions/FirstExample.java
+++ kodilla_stream/src/main/java/com/kodilla/stream/exceptions/FirstExample.java
@@ -1,13 +1,10 @@
 package com.kodilla.stream.exceptions;
 
 public class FirstExample {
-
-    public class FirstExample {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
             String username = "John Smith";
             String[] nameParts = username.split(" ");
             System.out.println("Firstname: " + nameParts[0]);
             System.out.println("Lastname: " + nameParts[1]);
         }
     }
-}
---
+++ kodilla_stream/src/test/java/com/kodilla/stream/exceptions/AirportRepositoryTestSuite.java
@@ -0,0 +1,30 @@
+
+package com.kodilla.stream.exceptions;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AirportRepositoryTestSuite {
+
+    @Test
+    public void testIsAirportInUse() throws AirportNotFoundException {
+        //given
+        AirportRepository airportRepository = new AirportRepository();
+        //when
+        boolean isWarsawInUse = airportRepository.isAirportInUse("Warsaw");
+        //then
+        assertTrue(isWarsawInUse);
+    }
+
+    @Test(expected = AirportNotFoundException.class)
+    public void testIsAirportInUse_withException() throws AirportNotFoundException {
+        //given
+        AirportRepository airportRepository = new AirportRepository();
+        //when
+        boolean isWarsawInUse = airportRepository.isAirportInUse("Vienna");
+        //then
+        assertTrue(isWarsawInUse);
+    }
+
+}
