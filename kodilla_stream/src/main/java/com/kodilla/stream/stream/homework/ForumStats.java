e341c558e008550e9d38ed33cbf815623fde2f6a
Dorota Czajkowska-Szubiczuk <dorota.szubiczuk@gmail.com>
2020-10-11 15:15:14 +0200

    Task 7.2

---
+++ kodilla_stream/src/main/java/com/kodilla/stream/stream/homework/ForumStats.java
@@ -0,0 +1,26 @@
+package com.kodilla.stream.stream.homework;
+
+import com.kodilla.stream.stream.UsersRepository;
+
+public class ForumStats {
+    public static void main(String[] args) {
+        double avgPostsOver40 = UsersRepository.getUsersList()
+                .stream()
+                .filter(user -> user.getAge() >= 40)
+                .map(user -> user.getNumberOfPosts())
+                .mapToInt(n->n)
+                .average()
+                .orElse(0.0);
+
+        System.out.println("Average post count for people at age 40 and above is : " + avgPostsOver40);
+
+        double avgPostsBelow40 = UsersRepository.getUsersList()
+                .stream()
+                .filter(user -> user.getAge() < 40)
+                .map(user -> user.getNumberOfPosts())
+                .mapToInt(n->n)
+                .average()
+                .orElse(0.0);
+        System.out.println("Average posts count for users below 40 years old is : " + avgPostsBelow40);
+    }
+}
