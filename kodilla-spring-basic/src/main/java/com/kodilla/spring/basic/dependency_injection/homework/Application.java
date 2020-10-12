95a72b276ffee7815d162a00a907d8a068e1afff
Dorota Czajkowska-Szubiczuk <dorota.szubiczuk@gmail.com>
2020-10-12 18:45:58 +0200

    12.10

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/homework/Application.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/homework/Application.java
@@ -1,9 +1,17 @@
 package com.kodilla.spring.basic.dependency_injection.homework;
 
 public class Application {
-
     public static void main(String[] args) {
-        ShippingCenter shippingCenter = new ShippingCenter(new DeliveryServiceImpl(), new NotificationServiceImpl());
+        DeliveryService deliveryService = new DeliveryService();
+        NotificationService dhlNotificationService = new DhlDeliveryService();
+        NotificationService dpdNotificationService = new DpdDeliveryService();
+
+        ShippingCenter shippingCenter = new ShippingCenter(deliveryService, dhlNotificationService);
         shippingCenter.sendPackage("Hill Street 11, New York", 18.2);
+        shippingCenter.sendPackage("Lake Street 22, Oregon", 41.8);
+
+        ShippingCenter shippingCenter1 = new ShippingCenter(deliveryService, dpdNotificationService);
+        shippingCenter1.sendPackage("Mountain Ave 2, Georgia", 2.45);
+        shippingCenter1.sendPackage("Forest Hill 8, Texas", 27.0);
     }
 }
