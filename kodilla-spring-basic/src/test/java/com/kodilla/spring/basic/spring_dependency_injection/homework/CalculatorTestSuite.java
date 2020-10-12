04233bee9439cae4bccf0609800797e4021065c1
Dorota Czajkowska-Szubiczuk <dorota.szubiczuk@gmail.com>
2020-10-12 20:20:21 +0200

    Task 9.2

---
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_dependency_injection/homework/CalculatorTestSuite.java
@@ -0,0 +1,48 @@
+package com.kodilla.spring.basic.spring_dependency_injection.homework;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootTest
+class CalculatorTestSuite {
+
+    ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic");
+    Calculator bean = context.getBean(Calculator.class);
+
+
+    @Test
+    public void shouldReturnCorrectAdditionResultAndDisplayResult(){
+        double result = bean.add(5.0,7.0);
+        Assertions.assertEquals(12.0, result);
+    }
+
+    @Test
+    public void shouldReturnCorrectSubtractionResultAndDisplayResult(){
+        double result = bean.subtract(10.0, 4.0);
+        Assertions.assertEquals(6.0, result);
+    }
+
+    @Test
+    public void shouldReturnCorrectMultiplicationResultAndDisplayResult(){
+        double result = bean.multiply(2.0, 3.5);
+        Assertions.assertEquals(7.0, result);
+    }
+
+    @Test
+    public void shouldReturnCorrectDivisionResultAndDisplayResult() throws ZeroUsageException {
+        double result = bean.divide(11.0, 2.0);
+        Assertions.assertEquals(5.5, result);
+    }
+
+    @Test
+    public void shouldReturnExceptionWhenDividingByZero()  {
+        Assertions.assertThrows(ZeroUsageException.class, ()->{
+            double result = bean.divide(11.0, 0);
+        });
+    }
+
+}
