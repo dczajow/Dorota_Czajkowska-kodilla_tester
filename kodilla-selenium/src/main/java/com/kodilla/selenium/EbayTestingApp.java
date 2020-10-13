5fe2d62f76a186d24e14d78cf00fb46d013ce978
Dorota Czajkowska-Szubiczuk <dorota.szubiczuk@gmail.com>
2020-10-13 20:35:07 +0200

    Task 13.3

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/EbayTestingApp.java
@@ -0,0 +1,19 @@
+package com.kodilla.selenium;
+
+        import org.openqa.selenium.By;
+        import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.WebElement;
+        import org.openqa.selenium.chrome.ChromeDriver;
+
+public class EbayTestingApp {
+        public static void main(String[] args) {
+            System.setProperty("webdriver.chrome.driver", "C:\\Selenium-drivers\\Chrome\\chromedriver.exe");
+            WebDriver driver = new ChromeDriver();
+            driver.get("https://www.ebay.com/");
+
+            WebElement inputField = driver.findElement(By.name("_nkw"));
+            inputField.sendKeys("Laptop");
+            inputField.submit();
+        }
+    }
+}
